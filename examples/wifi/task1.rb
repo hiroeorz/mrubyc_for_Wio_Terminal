94b36920981d97abb53094704e5109dc34d62bbe
Shin Hiroe <hiroe.orz@gmail.com>
2020-06-13 19:02:27 +0900

    TCP Client

--- examples/wifi/task1.rb
+++ examples/wifi/task1.rb
@@ -3,8 +3,8 @@ puts "WiFi for mruby/c"
 #-----------------------------------
 # Set your WiFi SSID and PASSWORD.
 #-----------------------------------
-SSID     = "************"
-PASSWORD = "************"
+SSID     = "90324BD2D660-5G"
+PASSWORD = "2215004388619"
 #-----------------------------------
 
 #
@@ -45,6 +45,29 @@ if wifi.connected?
   buzzer.buzz(128)
   sleep 1
   buzzer.buzz(0)
+end
+
+host = "192.168.3.10"
+port = 8080
+
+while true
+  if wifi.connect_to_server(host, port)
+    puts "OK connected to http server #{host}, #{port}"
+  else
+    puts "ERROR faild to connect to http server #{host}, #{port}"
+  end
+
+  wifi.write("GET /index.html HTTP/1.1\n\n")
+  puts "OK wifi is available."
+
+  60.times do
+    break if wifi.available > 0
+    sleep 1
+  end
+
+  str = wifi.read_string()
+  puts str
+  sleep 5
 end
 
 loop do; end
--- src/hal/ext_wifi.cpp
+++ src/hal/ext_wifi.cpp
@@ -11,6 +11,7 @@
 #include "ext.h"
 
 #define MRUBY_WIFI_STA 0
+WiFiClient *wifi_client = NULL;
 
 static void class_wifi_initialize(mrb_vm *vm, mrb_value *v, int argc)
 {
@@ -19,6 +20,9 @@ static void class_wifi_initialize(mrb_vm *vm, mrb_value *v, int argc)
     SET_NIL_RETURN();
     return;
   }
+
+  WiFi.mode(WIFI_STA); // default
+  wifi_client = new WiFiClient();
 }
 
 
@@ -143,6 +147,112 @@ static void class_wifi_local_ip(mrb_vm *vm, mrb_value *v, int argc)
   SET_RETURN(str);
 }
 
+static void class_wifi_connect(mrb_vm *vm, mrb_value *v, int argc)
+{
+  if (argc != 2) {
+    DEBUG_PRINTLN("invalid argc");
+    SET_NIL_RETURN();
+    return;
+  }
+
+  uint8_t *host = GET_STRING_ARG(1);
+  int port = GET_INT_ARG(2);
+  char debugStr[64];
+
+  if (wifi_client != NULL && wifi_client->connected()) {
+    wifi_client->stop();
+  }
+
+  wifi_client = new WiFiClient();
+
+  if (!wifi_client->connect((const char *)host, port)) {
+    sprintf(debugStr, "Connection failed: %s : %d", host, port);
+    DEBUG_PRINTLN(debugStr);
+    SET_FALSE_RETURN();
+    return;
+  }
+
+  SET_TRUE_RETURN();
+}
+
+static void class_wifi_available(mrb_vm *vm, mrb_value *v, int argc)
+{
+  if (argc != 0) {
+    DEBUG_PRINTLN("invalid argc");
+    SET_NIL_RETURN();
+    return;
+  }
+
+  int available = wifi_client->available();
+  SET_INT_RETURN(available);
+}
+
+static void class_wifi_write(mrb_vm *vm, mrb_value *v, int argc)
+{
+  if (argc != 1) {
+    DEBUG_PRINTLN("invalid argc");
+    SET_NIL_RETURN();
+    return;
+  }
+
+  uint8_t *str = GET_STRING_ARG(1);
+  wifi_client->print((const char*)str);
+  SET_TRUE_RETURN();
+}
+
+static void class_wifi_print(mrb_vm *vm, mrb_value *v, int argc)
+{
+  if (argc != 1) {
+    DEBUG_PRINTLN("invalid argc");
+    SET_NIL_RETURN();
+    return;
+  }
+
+  uint8_t *str = GET_STRING_ARG(1);
+  wifi_client->print((const char*)str);
+  SET_TRUE_RETURN();
+}
+
+static void class_wifi_read_string(mrb_vm *vm, mrb_value *v, int argc)
+{
+  if (argc != 0) {
+    DEBUG_PRINTLN("invalid argc");
+    SET_NIL_RETURN();
+    return;
+  }
+
+  String line = wifi_client->readString();
+  mrbc_value str = mrbc_string_new_cstr(vm, line.c_str());
+  SET_RETURN(str);
+}
+
+static void class_wifi_stop(mrb_vm *vm, mrb_value *v, int argc)
+{
+  if (argc != 0) {
+    DEBUG_PRINTLN("invalid argc");
+    SET_NIL_RETURN();
+    return;
+  }
+
+  wifi_client->stop();
+  SET_TRUE_RETURN();
+}
+
+static void class_wifi_is_server_connected(mrb_vm *vm, mrb_value *v, int argc)
+{
+  if (argc != 0) {
+    DEBUG_PRINTLN("invalid argc");
+    SET_NIL_RETURN();
+    return;
+  }
+
+  if (wifi_client->connected()) {
+    SET_TRUE_RETURN();
+  } else {
+    SET_FALSE_RETURN();
+  }
+}
+
 void define_wifi_class()
 {
   mrb_class *class_wifi;
@@ -156,4 +266,12 @@ void define_wifi_class()
   mrbc_define_method(0, class_wifi, "begin", class_wifi_begin);
   mrbc_define_method(0, class_wifi, "connected?", class_wifi_is_connected);
   mrbc_define_method(0, class_wifi, "local_ip", class_wifi_local_ip);
+  mrbc_define_method(0, class_wifi, "connect_to_server", class_wifi_connect);
+  mrbc_define_method(0, class_wifi, "available", class_wifi_available);
+  mrbc_define_method(0, class_wifi, "write", class_wifi_write);
+  mrbc_define_method(0, class_wifi, "print", class_wifi_print);
+  mrbc_define_method(0, class_wifi, "read_string", class_wifi_read_string);
+  mrbc_define_method(0, class_wifi, "stop", class_wifi_stop);
+  mrbc_define_method(0, class_wifi, "server_connected?", class_wifi_is_server_connected);
 }
+
